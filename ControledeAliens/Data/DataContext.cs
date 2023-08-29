05117c397842c87bff2af1c56f61cc2dd2dc28a7
Vinicius <viniciusgrossert@gmail.com>
2023-08-29 16:21:12 -0300

    Criação da pasta Data com a classe DataContext, Builder - Program,e DefaultConnection

---
+++ ControledeAliens/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace ControledeAliens.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Power> Powers { get; set; }
+        public DbSet<Planet> Planets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
