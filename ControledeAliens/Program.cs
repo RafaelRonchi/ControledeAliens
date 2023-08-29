388bfcf6c19e0c6b4dda2c91f46ff42ad0554b51
Vinicius <viniciusgrossert@gmail.com>
2023-08-29 16:22:29 -0300

    Revert "Criação da pasta Data com a classe DataContext, Builder - Program,e DefaultConnection"
    This reverts commit 05117c397842c87bff2af1c56f61cc2dd2dc28a7.

--- ControledeAliens/Program.cs
+++ ControledeAliens/Program.cs
@@ -1,6 +1,3 @@
-using ControledeAliens.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,13 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IAlienService, AlienService>();
-builder.Services.AddScoped<IPlanetService, PlanetService>();
-builder.Services.AddScoped<IPowerService, PowerService>();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 var app = builder.Build();
 
