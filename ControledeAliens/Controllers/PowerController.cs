90ff25ec136474e47b87c007ef073376784ac67d
Daniel Alves <daniel.bnu07@gmail.com>
2023-08-30 14:37:20 -0300

    service/controller power implemented

---
+++ ControledeAliens/Controllers/PowerController.cs
@@ -0,0 +1,66 @@
+﻿using ControledeAliens.Models;
+using ControledeAliens.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControledeAliens.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PowerController :ControllerBase
+    {
+        private readonly IPowerService _powerService;
+
+        public PowerController (IPowerService powerService)
+        {
+            _powerService = powerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPowers()
+        {
+            var powers = await _powerService.GetAllPowersAsync();
+            return Ok(powers);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetPowerById(int id)
+        {
+            var power = await _powerService.GetPowerByIdAsync(id);
+
+            if (power == null) return NotFound();
+
+            return Ok(power);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePower([FromBody] Power power)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState); 
+
+
+            await _powerService.CreatePowerAsync(power);
+            return CreatedAtAction("GetPowerById", new { id = power.Id }, power);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdatePower(int id, [FromBody] Power power)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (id != power.Id) return BadRequest();
+
+            await _powerService.UpdatePowerAsync(id, power);
+            return Ok(power);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeletePower(int id)
+        {
+            await _powerService.DeletePowerAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ ControledeAliens/Services/PowerService.cs
@@ -0,0 +1,52 @@
+﻿using ControledeAliens.Data;
+using ControledeAliens.Models;
+using ControledeAliens.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControledeAliens.Services
+{
+    public class PowerService : IPowerService
+    {
+        private readonly DataContext _dbContext;
+        public PowerService(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Power>> GetAllPowersAsync()
+        {
+            return await _dbContext.Powers.ToListAsync();
+        }
+
+        public async Task<Power> GetPowerByIdAsync(int id)
+        {
+            return await _dbContext.Powers.FindAsync(id);
+        }
+
+        public async Task CreatePowerAsync(Power power)
+        {
+            _dbContext.Powers.Add(power);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdatePowerAsync(int id, Power power)
+        {
+            if (id != power.Id)
+            {
+                throw new ArgumentException("IDs não correspondem.");
+            }
+
+            _dbContext.Entry(power).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeletePowerAsync(int id)
+        {
+            var power = await _dbContext.Powers.FindAsync(id);
+            if (power == null) return;
+
+            _dbContext.Powers.Remove(power);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
